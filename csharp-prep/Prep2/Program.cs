a301d8d04389a57a883c8fc6ca1658e0ed03132e
unknown <dmalasky@byui.edu>
2023-09-16 20:23:12 -0600

    Completed prep 1 and 2.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What was your grade? ");
+        string grade = Console.ReadLine();
+        int gradeNum = int.Parse(grade); 
+
+        string letter = " "; 
+
+        if (gradeNum > 89 )
+        {
+            letter = "A";
+        }
+        else if (gradeNum > 79)
+        {
+            letter = "B";
+        }
+          else if (gradeNum > 69)
+        {
+            letter = "C";
+        }
+          else if (gradeNum > 59)
+        {
+            letter = "D";
+        }
+          else if (gradeNum < 60)
+        {
+            letter = "F";
+        }
+
+        string results = " ";
+
+          if (gradeNum <= 69  )
+        {
+            results = "You failed the class, You'll have a headstart next year.";
+        }
+        else if (gradeNum > 69)
+        {
+            results = "You Passed, Congrats!";
+        }
+
+        Console.WriteLine($"Grade: {letter} \n{results}");
     }
 }
